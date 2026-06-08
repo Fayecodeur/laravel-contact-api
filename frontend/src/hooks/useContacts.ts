36231ed96355a0b8a577039211dbb9a93451ca1c
Demba Faye <fayecodeur99@gmail.com>
2026-06-08 15:09:53 +0000

    Hook pour la Recupération d'un contact

--- frontend/src/hooks/useContacts.ts
+++ frontend/src/hooks/useContacts.ts
@@ -1,9 +1,9 @@
 import { useQuery } from "@tanstack/react-query";
-import { getContact } from "../api/contactApi";
+import { getContacts } from "../api/contactApi";
 
 export const useContacts = () => {
   return useQuery({
     queryKey: ["contacts"],
-    queryFn: getContact,
+    queryFn: getContacts,
   });
 };
