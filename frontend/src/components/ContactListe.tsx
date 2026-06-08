96b42fe1b4a3099f66abc966557d31a7ad48942d
Demba Faye <fayecodeur99@gmail.com>
2026-06-08 15:09:33 +0000

    Recupération d'un contact

--- frontend/src/components/ContactListe.tsx
+++ frontend/src/components/ContactListe.tsx
@@ -1,16 +1,22 @@
+import { Link } from "react-router-dom";
 import type { Contact } from "../types/contact";
 
 type ContactProps = {
   contact: Contact;
 };
 export default function ContactListe({
-  contact: { name, phone, address },
+  contact: { id, name, phone, address },
 }: ContactProps) {
   return (
     <tr>
       <td> {name} </td>
       <td> {phone} </td>
       <td> {address} </td>
+      <td>
+        <Link className="btn btn-info btn-sm" to={`/contacts/${id}`}>
+          Voir
+        </Link>
+      </td>
     </tr>
   );
 }
