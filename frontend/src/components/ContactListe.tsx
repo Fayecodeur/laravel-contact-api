700548b773f3ef242b371ea22f12ad574ecfadd0
Demba Faye <fayecodeur99@gmail.com>
2026-06-08 15:49:01 +0000

    Suppression d'un contact

--- frontend/src/components/ContactListe.tsx
+++ frontend/src/components/ContactListe.tsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import type { Contact } from "../types/contact";
+import { useDeleteContact } from "../hooks/useDeleteContact";
 
 type ContactProps = {
   contact: Contact;
@@ -7,15 +8,23 @@ type ContactProps = {
 export default function ContactListe({
   contact: { id, name, phone, address },
 }: ContactProps) {
+  const { mutate: deleteContact, isPending } = useDeleteContact();
   return (
     <tr>
       <td> {name} </td>
       <td> {phone} </td>
       <td> {address} </td>
-      <td>
-        <Link className="btn btn-info btn-sm" to={`/contacts/${id}`}>
+      <td className="">
+        <Link className="btn btn-info btn-sm me-2" to={`/contacts/${id}`}>
           Voir
         </Link>
+        <button
+          disabled={isPending}
+          onClick={() => deleteContact(id)}
+          className="btn btn-danger btn-sm"
+        >
+          {isPending ? "Suppression" : "Supprimer"}
+        </button>
       </td>
     </tr>
   );
