097b88e56ee1f1ed505944539eba33b770b00e99
Demba Faye <fayecodeur99@gmail.com>
2026-06-08 15:09:19 +0000

    Recupération d'un contact

--- frontend/src/api/contactApi.ts
+++ frontend/src/api/contactApi.ts
@@ -4,7 +4,7 @@ const api = axios.create({
   baseURL: "http://127.0.0.1:8000/api",
 });
 
-export const getContact = async (): Promise<Contact[]> => {
+export const getContacts = async (): Promise<Contact[]> => {
   const response = await api.get("/contacts");
   return response.data.data;
 };
@@ -12,4 +12,9 @@ export const getContact = async (): Promise<Contact[]> => {
 export const createContact = async (data: Omit<Contact, "id">) => {
   const response = await api.post<Contact>("/contacts", data);
   return response.data;
+};
+
+export const getContactById = async (id: number): Promise<Contact> => {
+  const response = await api.get(`/contacts/${id}`);
+  return response.data.data;
 };
