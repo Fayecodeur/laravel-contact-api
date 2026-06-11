23ada421910b8b4a17cc2e520b9535b36d413752
Demba Faye <fayecodeur99@gmail.com>
2026-06-11 18:47:05 +0000

    Affichage des contacts

--- frontend/src/components/ContactListe.tsx
+++ frontend/src/components/ContactListe.tsx
@@ -5,25 +5,46 @@ import { useDeleteContact } from "../hooks/useDeleteContact";
 type ContactProps = {
   contact: Contact;
 };
+
 export default function ContactListe({
   contact: { id, name, phone, address },
 }: ContactProps) {
   const { mutate: deleteContact, isPending } = useDeleteContact();
+
+  const handleDelete = () => {
+    const confirmDelete = window.confirm("Voulez-vous supprimer ce contact ?");
+
+    if (confirmDelete) {
+      deleteContact(id);
+    }
+  };
+
   return (
     <tr>
-      <td> {name} </td>
-      <td> {phone} </td>
-      <td> {address} </td>
-      <td className="">
+      <td>{name}</td>
+
+      <td>{phone}</td>
+
+      <td>{address}</td>
+
+      <td>
         <Link className="btn btn-info btn-sm me-2" to={`/contacts/${id}`}>
           Voir
         </Link>
+
+        <Link
+          className="btn btn-warning btn-sm me-2"
+          to={`/contacts/${id}/edit`}
+        >
+          Modifier
+        </Link>
+
         <button
           disabled={isPending}
-          onClick={() => deleteContact(id)}
+          onClick={handleDelete}
           className="btn btn-danger btn-sm"
         >
-          {isPending ? "Suppression" : "Supprimer"}
+          {isPending ? "Suppression..." : "Supprimer"}
         </button>
       </td>
     </tr>
