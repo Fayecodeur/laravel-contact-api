5b33cda0bf76f97d11c090f7cc210a75601a3d25
Demba Faye <fayecodeur99@gmail.com>
2026-06-11 18:45:18 +0000

    Ajout du route edit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import ContactsPage from "./pages/ContactsPage";
 import { Toaster } from "react-hot-toast";
 import ContactShowPage from "./pages/ContactShowPage";
+import ContactEditPage from "./pages/ContactEditPage";
 
 function App() {
   return (
@@ -10,8 +11,12 @@ function App() {
 
       <Routes>
         <Route path="/" element={<ContactsPage />} />
+
         <Route path="/contacts" element={<ContactsPage />} />
-        <Route path={"/contacts/:id"} element={<ContactShowPage />} />
+
+        <Route path="/contacts/:id" element={<ContactShowPage />} />
+
+        <Route path="/contacts/:id/edit" element={<ContactEditPage />} />
       </Routes>
     </>
   );
